973b3d7dca0157ab2b7aab5bb0a203b8f96af401
Suufian <soufian.ben.bouker@est.fib.upc.edu>
2016-04-30 22:41:49 +0200

    Anadida gema y funcionalidad de rails_admin. Anadida gema omniauth, a falta de implementacion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,14 @@
 class SessionsController < ApplicationController
-  def new
-  end
 
   def create
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
-    end
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_path
   end
+
 end
