091c88f26c6efb7f61fbb6baf717b3c1b3ae000f
Suufian <soufian.ben.bouker@est.fib.upc.edu>
2016-05-03 12:41:21 +0200

    Facebook image size changed.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :facebook, '1560477190918320', '6bfcab1d0eb302283a76ec02782caa80',
   {
-      :image_size => 'original'
+      :image_size => 'large'
   }
 
   provider :google_oauth2, '118581444224-gqiag2q8ivqpa0m8o1rit1u91fgahgh7.apps.googleusercontent.com',
